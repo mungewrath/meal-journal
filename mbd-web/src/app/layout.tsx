9de3c6ec1f6cfffca1ab6641957ffc5eb844f5fe
Dan Piorkowski <danpiorkowski@gmail.com>
2025-02-27 20:16:46 -0800

    Install Material UI

--- mbd-web/src/app/layout.tsx
+++ mbd-web/src/app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
+// Roboto font needed for Material-UI
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 export const metadata: Metadata = {
   title: "My Belly's Diary",

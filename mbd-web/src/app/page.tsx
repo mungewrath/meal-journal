ecb568ec926634239f941828c2a8200f90665d89
Dan Piorkowski <danpiorkowski@gmail.com>
2025-02-27 16:50:54 -0800

    Install Prettier, Fix ESLint server errors, Include root page's CSS

--- mbd-web/src/app/page.tsx
+++ mbd-web/src/app/page.tsx
@@ -1,13 +1,12 @@
-
+import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <main>
-        <h1>My Belly's Diary</h1>
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>My Belly&apos;s Diary</h1>
       </main>
-      <footer>
-      </footer>
+      <footer className={styles.footer}></footer>
     </div>
   );
 }

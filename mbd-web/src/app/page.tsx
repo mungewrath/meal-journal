1f16ece5dcb229d0d1a28c92cdb991ec3526cf8f
Matthew Unrath <matthew.unrath@gmail.com>
2025-03-22 16:39:52 -0700

    Mixups

--- mbd-web/src/app/page.tsx
+++ mbd-web/src/app/page.tsx
@@ -10,10 +10,12 @@ import LoginPage from "@/ui/LoginPage";
 export default function Home() {
   const cognitoAuthConfig = {
     authority:
-      "https://cognito-idp.us-west-2.amazonaws.com/us-west-2_gholbnsH6",
-    client_id: "2sr1932m3b9l641vdo6151uoor",
+      "https://cognito-idp.us-west-2.amazonaws.com/us-west-2_3vQ5Mu7VR", // stage
+    // "https://cognito-idp.us-west-2.amazonaws.com/us-west-2_gholbnsH6", // prod
+    // client_id: "2sr1932m3b9l641vdo6151uoor", //prod
+    client_id: "6li7j91bl741p9st10pbvmgadm", //stage
     // redirect_uri: "http://localhost:3000",
-    redirect_uri: "https://d1l9mogzhjoud6.cloudfront.net",
+    redirect_uri: "https://d1l9mogzhjoud6.cloudfront.net", // stage
     response_type: "code",
     scope: "openid profile email",
   };

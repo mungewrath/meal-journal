4d433a6d5663437f6eafc2eb76dcf553478e3d95
Matthew Unrath <matthew.unrath@gmail.com>
2025-03-22 20:43:46 -0700

    Pass environment specific configs for UI builds

--- mbd-web/src/ui/LoginPage.tsx
+++ mbd-web/src/ui/LoginPage.tsx
@@ -29,11 +29,13 @@ function LoginPage() {
       <div className="min-h-screen bg-gray-50">
         <div className="max-w-2xl mx-auto p-4 space-y-4">
           Welcome, {auth.user?.profile["cognito:username"] as string}
-          <div className="space-y-2 overflow-x-auto">
-            <pre> ID Token: {auth.user?.id_token} </pre>
-            <pre> Access Token: {auth.user?.access_token} </pre>
-            <pre> Refresh Token: {auth.user?.refresh_token} </pre>
-          </div>
+          {process.env.NEXT_PUBLIC_DEBUG_AUTH === "true" && (
+            <div className="space-y-2 overflow-x-auto">
+              <pre> ID Token: {auth.user?.id_token} </pre>
+              <pre> Access Token: {auth.user?.access_token} </pre>
+              <pre> Refresh Token: {auth.user?.refresh_token} </pre>
+            </div>
+          )}
         </div>
       </div>
     );

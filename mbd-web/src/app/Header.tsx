f23cecde8d85020c2540791cbac680599f4f96c1
Matthew Unrath <matthew.unrath@gmail.com>
2025-03-27 22:15:40 -0700

    PR feedback

--- mbd-web/src/app/Header.tsx
+++ mbd-web/src/app/Header.tsx
@@ -3,12 +3,10 @@
 import { AppBar, Toolbar, IconButton, Typography, Button } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import { useAuth } from "react-oidc-context";
-// import { useSession, signIn, signOut } from "next-auth/react";
 
 export const Header = () => {
-  // const { data: session } = useSession();
   const auth = useAuth();
-  
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -23,17 +21,6 @@ export const Header = () => {
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           My Belly&apos;s Diary
         </Typography>
-        {/* {session ? (
-          <>
-            Signed in as {session?.user?.email} <br />
-            <button onClick={() => signOut()}>Sign out</button>
-          </>
-        ) : (
-          <>
-            Not signed in <br />
-            <button onClick={() => signIn()}>Sign in</button>
-          </>
-        )} */}
         {auth.isAuthenticated && (
           <>
             Oh hai {auth.user?.profile["cognito:username"]}

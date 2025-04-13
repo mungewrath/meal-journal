9a7327972a3b09a7dfc09ffb3173e4563cfbdd27
Matthew Unrath <matthew.unrath@gmail.com>
2025-04-12 21:46:50 -1000

    Fix the test and add a constraint

--- backend/tests/test_api.py
+++ backend/tests/test_api.py
@@ -1,6 +1,8 @@
 from unittest.mock import patch, MagicMock
 from uuid import uuid4
 
+from foods.food import MbdFood
+
 
 @patch("app.main.MbdFoodList.get")
 async def test_create_food__returns_new_food(mock_get, client, mock_get_user_id):
@@ -41,8 +43,12 @@ async def test_create_food__returns_bad_request_if_food_exists(
     # Mock the food list with an existing food
     mock_food_list = MagicMock()
     existing_food_id = str(uuid4())
+    existing_food = MbdFood()
+    existing_food.food_id = existing_food_id
+    existing_food.name = "Blueberries"
+    existing_food.thumbnail = "🫐"
     mock_food_list.foods = [
-        MagicMock(food_id=existing_food_id, name="Blueberries", thumbnail="🫐")
+        existing_food,
     ]
     mock_get.return_value = mock_food_list
 
@@ -59,8 +65,41 @@ async def test_create_food__returns_bad_request_if_food_exists(
 
     # Verify the response
     assert response.status_code == 400
-    data = response.json()
-    assert data["detail"] == "Food with the specified name already exists."
+
+    # Verify no new food was added to the list
+    assert len(mock_food_list.foods) == 1
+    mock_food_list.save.assert_not_called()
+
+
+@patch("app.main.MbdFoodList.get")
+async def test_create_food__returns_bad_request_if_food_exists_ignoring_case(
+    mock_get, client, mock_get_user_id
+):
+    # Mock the food list with an existing food
+    mock_food_list = MagicMock()
+    existing_food_id = str(uuid4())
+    existing_food = MbdFood()
+    existing_food.food_id = existing_food_id
+    existing_food.name = "blueberries"
+    existing_food.thumbnail = "🫐"
+    mock_food_list.foods = [
+        existing_food,
+    ]
+    mock_get.return_value = mock_food_list
+
+    # Make the request with the same food name
+    response = client.post(
+        "/foods",
+        headers={"Authorization": "Bearer test-token"},
+        json={
+            "id": str(uuid4()),
+            "name": "BLUEBERRIES",
+            "thumbnail": "🫐",
+        },
+    )
+
+    # Verify the response
+    assert response.status_code == 400
 
     # Verify no new food was added to the list
     assert len(mock_food_list.foods) == 1

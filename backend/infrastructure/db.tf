a7f4a1a4411e70daf10b84346ca4e6856fe8a252
Matthew Unrath <matthew.unrath@gmail.com>
2025-05-03 17:02:38 -0700

    Try setting deletion protection for DynamoDB tables

--- backend/infrastructure/db.tf
+++ backend/infrastructure/db.tf
@@ -1,7 +1,8 @@
 resource "aws_dynamodb_table" "mbd_user_preferences" {
-  name         = "mbd_user_preferences"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "user_id"
+  name                        = "mbd_user_preferences"
+  billing_mode                = "PAY_PER_REQUEST"
+  hash_key                    = "user_id"
+  deletion_protection_enabled = true
 
   attribute {
     name = "user_id"
@@ -10,10 +11,11 @@ resource "aws_dynamodb_table" "mbd_user_preferences" {
 }
 
 resource "aws_dynamodb_table" "mbd_meals" {
-  name         = "mbd_meals"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "user_id"
-  range_key    = "date_time"
+  name                        = "mbd_meals"
+  billing_mode                = "PAY_PER_REQUEST"
+  hash_key                    = "user_id"
+  range_key                   = "date_time"
+  deletion_protection_enabled = true
 
   attribute {
     name = "user_id"
@@ -28,9 +30,10 @@ resource "aws_dynamodb_table" "mbd_meals" {
 }
 
 resource "aws_dynamodb_table" "mbd_foods" {
-  name         = "mbd_foods"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "user_id"
+  name                        = "mbd_foods"
+  billing_mode                = "PAY_PER_REQUEST"
+  hash_key                    = "user_id"
+  deletion_protection_enabled = true
 
   attribute {
     name = "user_id"
@@ -39,10 +42,11 @@ resource "aws_dynamodb_table" "mbd_foods" {
 }
 
 resource "aws_dynamodb_table" "mbd_symptoms" {
-  name         = "mbd_symptoms"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "user_id"
-  range_key    = "date_time"
+  name                        = "mbd_symptoms"
+  billing_mode                = "PAY_PER_REQUEST"
+  hash_key                    = "user_id"
+  range_key                   = "date_time"
+  deletion_protection_enabled = true
 
   attribute {
     name = "user_id"
